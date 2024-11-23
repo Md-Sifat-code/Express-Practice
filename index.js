b661cde2d1cc4f80e21f6501b2bbcac852d698e5
Md-Sifat-code <codewithsifat4@gmail.com>
2024-11-23 23:37:49 +0600

    added json data for clint side

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
-
+const phones = require('./phones.json')
 const app = express()
 const port =7000
 
 app.get('/',(req,res)=>{
     res.send('Hello you are the best mothertfucker')
 
+})
+app.get('/phones',(req,res)=>{
+    res.send(phones)
+})
+
+app.get('/phones/:id',(req,res)=>{
+    const id = parseInt(req.params.id)
+    const phone = phones.find(phone => phone.id === id)||{}
+    req.send(phone)
 })
 
 app.get('/motherfucker',(req,res)=>{
